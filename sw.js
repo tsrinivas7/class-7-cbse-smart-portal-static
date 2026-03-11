98d090283006eea75fe7c82ebd5d362b6281543b
Srinivas T <tsrinivas7@users.noreply.github.com>
2026-03-12 01:53:59 +0530

    reverting by 2 commits to make it work

--- sw.js
+++ sw.js
@@ -21,7 +21,8 @@ const ASSETS = [
     "./maths/chapter_09.html",
     "./maths/chapter_08.html",
     "./maths/chapter_07.html",
-    "./maths/chapter_06.html"
+    "./maths/chapter_06.html",
+    "./computerscience/chapter_09.html"
 ];
 const VERSION_URL = './version.json';
 
